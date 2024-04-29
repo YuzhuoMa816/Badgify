8eb488d3adf93b659b34d10d7067ee81a5fd0eae
Zoe Chen <zoe.chen@capgemini.com>
2024-04-29 13:24:19 +1000

    update crud on user data

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -6,8 +6,8 @@ import '../../utils/exceptions/firebase_exceptions.dart';
 Future<void> saveUserRecord(UserModel user) async {
   try {
     /// Repository class for user-related operations.
-    FirebaseFirestore _db = FirebaseFirestore.instance;
-    await _db.collection("Users").doc(user.uid).set(user.toJson());
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    await db.collection("Users").doc(user.uid).set(user.toJson());
   } on FirebaseException catch (e) {
     throw BFirebaseException(e.code).message;
   } catch (e) {
@@ -16,10 +16,10 @@ Future<void> saveUserRecord(UserModel user) async {
 }
 
 /// Function to fetch user details based on user ID.
-Future<UserModel> fetchUserDetails() async {
+Future<UserModel> fetchUserDetails(String uid) async {
   try {
-    FirebaseFirestore _db = FirebaseFirestore.instance;
-    final documentSnapshot = await _db.collection("Users").doc().get();
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    final documentSnapshot = await db.collection("Users").doc(uid).get();
     if (documentSnapshot.exists) {
       return UserModel.fromSnapshot(documentSnapshot);
     } else {
@@ -35,8 +35,8 @@ Future<UserModel> fetchUserDetails() async {
 /// Function to update user data in Firestore.
 Future<void> updateUserDetails(UserModel updatedUser) async {
   try {
-    FirebaseFirestore _db = FirebaseFirestore.instance;
-    await _db.collection("Users").doc(updatedUser.uid).update(updatedUser.toJson());
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    await db.collection("Users").doc(updatedUser.uid).update(updatedUser.toJson());
   } on FirebaseException catch (e) {
     throw BFirebaseException(e.code).message;
   } catch (e) {
@@ -47,8 +47,8 @@ Future<void> updateUserDetails(UserModel updatedUser) async {
 /// Function to remove user data from Firestore.
 Future<void> removeUserRecord(String userId) async {
   try {
-    FirebaseFirestore _db = FirebaseFirestore.instance;
-    await _db.collection("Users").doc(userId).delete();
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    await db.collection("Users").doc(userId).delete();
   } on FirebaseException catch (e) {
     throw BFirebaseException(e.code).message;
   } catch (e) {
